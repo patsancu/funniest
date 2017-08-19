eeab0255f02bf6b0d5e0a37ade60dfb285dc6670
Patrick <patricksanzo@gmail.com>
2017-08-19 20:07:38 +0200

    changed to my gh repo, since the original 404ed

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='funniest',
       version='0.1',
       description='The funniest joke in the world',
-      url='http://github.com/storborg/funniest',
+      url='http://github.com/patsancu/funniest',
       author='Flying Circus',
       author_email='flyingcircus@example.com',
       license='MIT',
